ac622dada66edbe39747be3e2b2ec0db99c33d33
PedroCNeto <pedrocastelarineto@gmail.com>
2025-02-27 23:02:06 -0300

    renomeando variaveis

--- Assets/BaiacuScript.cs
+++ Assets/BaiacuScript.cs
@@ -6,42 +6,39 @@ public class BaiacuScript : MonoBehaviour
     public BreathingEffect breath;
     public SpriteRenderer mySprite;
 
-    public float amplitude = 0.5f; 
-    public float speed = 1.4f;
-    public float inflateSpeed = 0.4f; 
-    public float maxScale = 25f; 
+    public float amplitude = 0.5f; // Amplitude do balanço do baiacu para cima e para baixo
+    public float velocidadeDeBalanco = 1.4f; // Velocidade que o baiacu se movimenta
+    public float velocidadeDeInflacao = 0.4f; // Velocidade que o baiacu vai inchar ou desinchar
+    public float tamanhoMaximo = 25f; // Tamanho maximo que ele pode chegar
 
+    private Vector3 posicaoInicial;
+    private Vector3 escalaInicial;
 
-    private float originalRotation;
-    private Vector3 startPosition;
-    private Vector3 originalScale;
-
-    private Color originalColor;
-    public Color targetColor = Color.magenta; // Cor de destino para a transição
-    private float colorChangeSpeed = 2f; // Velocidade da transição de cor
+    private Color corOriginal;
+    public Color corAlvo = Color.magenta; // Cor que o baiacu vai ficar quando estiver inflado ao maximo
+    private float velocidadeDaMudancaDeCor = 2f; // Velocidade da transição de cor
 
     private bool isInflating = false; // Flag para controlar a inflação
 
     // Parâmetros do balanço
-    public float balanceSpeed = 2f; // Velocidade do balanço
-    public float balanceAmount = 10f; // Amplitude do balanço
+    public float velocidadeBalanco = 2f; // Velocidade do balanço
+    public float amplitudeBalanco = 10f; // Amplitude do balanço
 
     public Animator animator;
 
     void Start()
     {
-        startPosition = transform.position;
-        originalScale = transform.localScale;
-        originalColor = mySprite.color;
-        originalRotation = transform.rotation.z;
+        posicaoInicial = transform.position;
+        escalaInicial = transform.localScale;
+        corOriginal = mySprite.color;
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         // Movimento de flutuação
-        float newY = startPosition.y + Mathf.Sin(Time.time * speed) * amplitude;
-        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+        float newY = posicaoInicial.y + Mathf.Sin(Time.time * velocidadeDeBalanco) * amplitude;
+        transform.position = new Vector3(posicaoInicial.x, newY, posicaoInicial.z);
 
         // Verifica se o jogador está pressionando a tecla espaço
         if (Input.GetKey(KeyCode.Space)) 
@@ -69,7 +66,7 @@ public class BaiacuScript : MonoBehaviour
     void inflateBaiacu()
     {
         // Infla o Baiacu de forma suave
-        float newScale = Mathf.Lerp(transform.localScale.x, maxScale, Time.deltaTime * inflateSpeed);
+        float newScale = Mathf.Lerp(transform.localScale.x, tamanhoMaximo, Time.deltaTime * velocidadeDeInflacao);
         transform.localScale = new Vector3(newScale, newScale, newScale);
         breath.animationTrigger("IsInflating");
         breath.setAnimValue("IsDeflating", 0);
@@ -77,9 +74,9 @@ public class BaiacuScript : MonoBehaviour
         // Muda a cor quando atingir o tamanho máximo
         if (transform.localScale.x >= 20f)
         {
-            mySprite.color = Color.Lerp(mySprite.color, targetColor, Time.deltaTime * colorChangeSpeed);
+            mySprite.color = Color.Lerp(mySprite.color, corAlvo, Time.deltaTime * velocidadeDaMudancaDeCor);
             // Faz o Baiacu balançar
-            float newRotation = Mathf.Sin(Time.time * balanceSpeed) * balanceAmount;
+            float newRotation = Mathf.Sin(Time.time * velocidadeBalanco) * amplitudeBalanco;
             transform.rotation = Quaternion.Euler(0, 0, newRotation);
         }
     }
@@ -87,13 +84,13 @@ public class BaiacuScript : MonoBehaviour
     void deflateBaiacu()
     {
         // Desinfla o Baiacu de forma suave
-        float newScale = Mathf.Lerp(transform.localScale.x, originalScale.x, Time.deltaTime * inflateSpeed);
+        float newScale = Mathf.Lerp(transform.localScale.x, escalaInicial.x, Time.deltaTime * velocidadeDeInflacao);
         transform.localScale = new Vector3(newScale, newScale, newScale);
 
         // Volta para a cor original quando o tamanho diminui
         if (transform.localScale.x < 20f)
         {
-            mySprite.color = Color.Lerp(mySprite.color, originalColor, Time.deltaTime * colorChangeSpeed);
+            mySprite.color = Color.Lerp(mySprite.color, corOriginal, Time.deltaTime * velocidadeDaMudancaDeCor);
         }
 
         if (transform.localScale.x > 16f){
